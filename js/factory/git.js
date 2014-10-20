9d7e3cf8f991248bbb2da3175377d9b53f909c4a
Mario Hsieh <mariohsieh@gmail.com>
2014-10-20 12:12:21 -0700

    added more commands to git

--- js/factory/git.js
+++ js/factory/git.js
@@ -16,6 +16,14 @@ angular.module("factoryGit", [])
 				command:	"git add",
 				example:	["git add .", "git add index.html"],
 				action: 	["add all files", "add specified file"]
+			},{
+				command:	"git commit",
+				example:	["git commit -m 'initial commit'"],
+				action: 	["save a project with message 'initial commit'"]
+			},{
+				command:	"git push",
+				example:	["git push origin master", "git push heroku master", "git push origin gh-pages"],
+				action: 	["push commits to github master branch", "deploy project to heroku", "deploy project to gh-pages branch"]
 			}
 			
 		];
